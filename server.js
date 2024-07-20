dfa6bb3be7aaf4588f6157d39e04f29f5aea556b
julien-35 <julien45.dubois@gmail.com>
2024-07-20 17:46:54 +0200

    Fix file path issues

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Écouter sur le port défini par Heroku ou un port local
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
